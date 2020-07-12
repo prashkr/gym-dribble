fe2a9588d69de39851d44dee0e0ba298db3e8b3b
prashant <prashant@falconx.io>
2020-07-13 04:17:30 +0530

    removing comment

--- gym_dribble/envs/dribble.py
+++ gym_dribble/envs/dribble.py
@@ -1,9 +1,3 @@
-"""
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
-"""
-
 import math
 import gym
 from gym import spaces, logger
